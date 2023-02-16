494597b403a8b82547d1559013b766b3bbb95b47
Vitalii Klimenko <caxaroook.vitaly@ya.ru>
2023-02-16 16:12:58 +0400

    6 practical work Fix ovrlay modal v1.2 after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,130 +1,145 @@
-import {initialCards} from "./cardsValue.js"
+import { initialCards } from "./cardsValue.js";
 
-// const
-  const template = document.querySelector("#template__card").content.querySelector(".elements__item");
-  const cardList = document.querySelector(".elements__item-list");
-  const sumbitBtnCard = document.querySelector(".popup__submit-btn_card");
-  const inputCardTitle = document.querySelector(".popup__input_card_title");
-  const inputCardLink = document.querySelector(".popup__input_card_link");
-  const popupCardBtn = document.querySelector(".profile__plus-btn");
-  const popupCard = document.querySelector(".popup_type_add-card");
-  const popupProfielBtn = document.querySelector(".profile__pencil-btn");
-  const popupProfile = document.querySelector(".popup_type_edit");
-  const inputProfileName = document.querySelector(".popup__input_element_name");
-  const inputProfileJob = document.querySelector(".popup__input_element_job");
-  const submitProfileBtn = document.querySelector(".popup__submit-btn_profile");
-  const nameFromProfile = document.querySelector(".profile__name");
-  const jobFromProfile = document.querySelector(".profile__describe");
-  const btnClosePopupProfile = document.querySelector(".popup__close-btn_type_profile");
-  const btnClosePopupCard = document.querySelector(".popup__close-btn_type_card");
-  const popupImg = document.querySelector(".popup_type_img");
-  const btnClosePopupImg = document.querySelector(".popup__close-btn_type_img");
-  const imgPopupImg = document.querySelector(".popup__img");
-  const popupCaption = document.querySelector(".popup__caption");
-  const formPopupProfile = document.querySelector(".popup__edit-form");
-  const formPopupCard = document.querySelector(".popup__add-form")
+const cardTemplate = document
+  .querySelector("#template__card")
+  .content.querySelector(".elements__item");
+const cardsContainer = document.querySelector(".elements__item-list");
+const sumbitBtnCard = document.querySelector(".popup__submit-btn_card");
+const inputCardTitle = document.querySelector(".popup__input_card_title");
+const inputCardLink = document.querySelector(".popup__input_card_link");
+const popupCardBtn = document.querySelector(".profile__plus-btn");
+const popupCard = document.querySelector(".popup_type_add-card");
+const popupProfielBtn = document.querySelector(".profile__pencil-btn");
+const popupProfile = document.querySelector(".popup_type_edit");
+const inputProfileName = document.querySelector(".popup__input_element_name");
+const inputProfileJob = document.querySelector(".popup__input_element_job");
+const submitProfileBtn = document.querySelector(".popup__submit-btn_profile");
+const nameFromProfile = document.querySelector(".profile__name");
+const jobFromProfile = document.querySelector(".profile__describe");
+const btnClosePopupProfile = document.querySelector(".popup__close-btn_type_profile");
+const btnClosePopupCard = document.querySelector(".popup__close-btn_type_card");
+const popupImg = document.querySelector(".popup_type_img");
+const btnClosePopupImg = document.querySelector(".popup__close-btn_type_img");
+const BtnsCommonClose = document.querySelectorAll(".popup__close-btn");
+const imgPopupImg = document.querySelector(".popup__img");
+const popupCaption = document.querySelector(".popup__caption");
+const formPopupProfile = document.querySelector(".popup__edit-form");
+const formPopupCard = document.querySelector(".popup__add-form");
 
 // funtions
 
-  function createCard(item){
-    const card = template.cloneNode(true);
-       card.querySelector(".elements__title").textContent = item.name;
-       const cardImg = card.querySelector(".elements__img");
-       cardImg.src = item.link;
-       cardImg.alt = item.name;
-       card.querySelector(".elements__delete-btn").addEventListener("click", () => {
-        card.remove()
-       });
-       const likeCardBtn = card.querySelector(".elements__like-btn");
-       likeCardBtn.addEventListener("click", () => {
-        likeCardBtn.classList.toggle("elements__like-btn_active")
-       });
-       const imgCard = card.querySelector(".elements__img");
-       imgCard.addEventListener("click", () => {
-        imgPopupImg.src = cardImg.src
-        imgPopupImg.alt = cardImg.alt
-        popupCaption.textContent = cardImg.alt
-        openModal(popupImg)
-       });
-       
-       return card;
-  }
-
-  function renderCard (items) {
-    const cards = items.map((item) => {
-    
-       return createCard(item);
-    })
-    cardList.prepend(...cards);
-  }
- 
-  function openModal(modal) {
-    modal.classList.add("popup_opened");
-  }
-
-  function closeModal(modal) {
-    modal.classList.remove("popup_opened");
-  }
-
-  function cleanPopupCardInput() {
-    closeModal(popupCard)
-    inputCardTitle.value = "";
-    inputCardLink.value = "";
-  }
-
-  function getInfoForInput () {
-    openModal(popupProfile)
-    inputProfileName.value = nameFromProfile.textContent;
-    inputProfileJob.value = jobFromProfile.textContent;
-  }
-
-  renderCard(initialCards);
-
-  // listeners
-
-  popupCardBtn.addEventListener("click", () => openModal(popupCard));
-  
-  btnClosePopupImg.addEventListener("click", () => closeModal(popupImg));
-
-  btnClosePopupProfile.addEventListener("click", () => closeModal(popupProfile));
-
-  popupProfielBtn.addEventListener("click", getInfoForInput);
-
-  btnClosePopupCard.addEventListener("click", () => closeModal(popupCard));
-
-  popupProfielBtn.addEventListener("click", () => openModal(popupProfile));
-
-  formPopupCard.addEventListener("submit", (event) =>{
-    event.preventDefault();
-    disableButton(sumbitBtnCard, "popup__button_disabled");
-    const title = inputCardTitle.value;
-    const link = inputCardLink.value;
-    const card = createCard({name: title, link: link});
-    cardList.prepend(card);
-    cleanPopupCardInput();
+function createCard(cardData) {
+  const card = cardTemplate.cloneNode(true);
+  card.querySelector(".elements__title").textContent = cardData.name;
+  const cardImg = card.querySelector(".elements__img");
+  cardImg.src = cardData.link;
+  cardImg.alt = cardData.name;
+  card.querySelector(".elements__delete-btn").addEventListener("click", () => {
+    card.remove();
+  });
+  const likeCardBtn = card.querySelector(".elements__like-btn");
+  likeCardBtn.addEventListener("click", () => {
+    likeCardBtn.classList.toggle("elements__like-btn_active");
+  });
+  const imgCard = card.querySelector(".elements__img");
+  imgCard.addEventListener("click", () => {
+    imgPopupImg.src = cardImg.src;
+    imgPopupImg.alt = cardImg.alt;
+    popupCaption.textContent = cardImg.alt;
+    openModal(popupImg);
   });
 
-  formPopupProfile.addEventListener("submit", (event) =>{
-    event.preventDefault();
-    disableButton(submitProfileBtn, "popup__button_disabled");
-    nameFromProfile.textContent = inputProfileName.value;
-    jobFromProfile.textContent = inputProfileJob.value;
-    closeModal(popupProfile);
-  });
+  return card;
+}
 
-  // close modal via key "esc"
-  document.addEventListener('keydown', function(evt) {
-    if( evt.key == "Escape"){
-      const modal = document.querySelector('.popup_opened');
-      closeModal(modal);
-    }
+function renderCard(renderCards) {
+  const cards = renderCards.map((item) => {
+    return createCard(item);
   });
+  cardsContainer.prepend(...cards);
+}
 
-// close modal via overlay
-  document.addEventListener( 'click', (evt) => {
-    const modal = document.querySelector('.popup_opened');
-    if(evt.target === modal) {
-      closeModal(modal);
-    }
-  });
+function openModal(modal) {
+  modal.classList.add("popup_opened");
+  modal.addEventListener("mousedown", closeViaOverlay);
+  document.addEventListener("keydown", closeViaEscape);
+}
 
+function closeModal(modal) {
+  modal.classList.remove("popup_opened");
+  modal.removeEventListener("mousedown", closeViaOverlay);
+  document.removeEventListener("keydown", closeViaEscape);
+}
+
+function closeViaEscape(evt) {
+  if (evt.key === "Escape") {
+    const openedModal = document.querySelector(".popup_opened");
+    closeModal(openedModal);
+  }
+}
+
+function closeViaOverlay(evt) {
+  const openedModal = document.querySelector(".popup_opened");
+  if (evt.target === openedModal) {
+    closeModal(openedModal);
+  }
+}
+
+function cleanPopupCardInput() {
+  closeModal(popupCard);
+  resetFormInputs(formPopupCard);
+}
+
+function openProfilePopup() {
+  openModal(popupProfile);
+  signProfileInputs();
+}
+function signProfileInputs() {
+  inputProfileName.value = nameFromProfile.textContent;
+  inputProfileJob.value = jobFromProfile.textContent;
+}
+
+function resetFormInputs(form) {
+  form.reset();
+}
+
+BtnsCommonClose.forEach((button) => {
+  const modal = button.closest(".popup");
+  button.addEventListener("click", () => closeModal(modal));
+});
+
+renderCard(initialCards);
+
+// listeners
+
+popupCardBtn.addEventListener("click", () => {
+  openModal(popupCard);
+});
+
+// btnClosePopupImg.addEventListener("click", () => closeModal(popupImg));
+
+// btnClosePopupProfile.addEventListener("click", () => closeModal(popupProfile));
+
+popupProfielBtn.addEventListener("click", openProfilePopup);
+
+// btnClosePopupCard.addEventListener("click", () => closeModal(popupCard));
+
+// popupProfielBtn.addEventListener("click", () => openModal(popupProfile));
+
+formPopupCard.addEventListener("submit", (event) => {
+  event.preventDefault();
+  disableButton(sumbitBtnCard, "popup__submit-btn_disabled");
+  const title = inputCardTitle.value;
+  const link = inputCardLink.value;
+  const card = createCard({ name: title, link: link });
+  cardsContainer.prepend(card);
+  cleanPopupCardInput();
+});
+
+formPopupProfile.addEventListener("submit", (event) => {
+  event.preventDefault();
+  disableButton(submitProfileBtn, "popup__submit-btn_disabled"); // считаю, что эта строчка нужна, так как без этой строки можно отправлять кучу раз одинаковые данные тем самый нагружать сайт Юрий Абарцумян просил это учитывать по возможности
+  nameFromProfile.textContent = inputProfileName.value;
+  jobFromProfile.textContent = inputProfileJob.value;
+  closeModal(popupProfile);
+});
--- scripts/validation.js
+++ scripts/validation.js
@@ -1,86 +1,103 @@
 const showInputError = (inputElement, errorElement, invalidInputClass) => {
-    inputElement.classList.add(invalidInputClass);
-    errorElement.textContent = inputElement.validationMessage;
-  }
-  
-  const hideInputError = (inputElement, errorElement, invalidInputClass) => {
-    inputElement.classList.remove(invalidInputClass);
-    errorElement.textContent = inputElement.validationMessage;
-  }
-  
-  const disableButton = (formSubmitButtonElement, disableSubmitButtonClass) => {
-    formSubmitButtonElement.classList.add(disableSubmitButtonClass);
-    formSubmitButtonElement.disabled = true;
-  }
-  
-  const enableButton = (formSubmitButtonElement, disableSubmitButtonClass) => {
-    formSubmitButtonElement.classList.remove(disableSubmitButtonClass);
-    formSubmitButtonElement.disabled = false;
-  }
-  
-  const toggleButtonState = (formSubmitButtonElement, disableSubmitButtonClass, buttonState) =>{
-    if(buttonState) {
-      disableButton(formSubmitButtonElement, disableSubmitButtonClass);
-    } else {
-      enableButton(formSubmitButtonElement, disableSubmitButtonClass);
-    }
-  }
-  
-  const checkInputValidity = (inputElement, errorElement, invalidInputClass) => {
-    if (inputElement.validity.valid) {
-      hideInputError(inputElement, errorElement, invalidInputClass);
-    } else {
-      showInputError(inputElement, errorElement, invalidInputClass);
-    }
-  }
-  
-  const hasInvalidInput = (inputList) => {
-    return inputList.some((input) => !input.validity.valid);
-  }
-  
-  const handlerFormInput = (
-    evt, form, invalidInputClass, formSubmitButtonElement, disableSubmitButtonClass, inputList) => {
-    const inputElement = evt.target;
-    const errorElement = form.querySelector(`.popup__input-error_field_${inputElement.name}`);
-    const buttonState = hasInvalidInput(inputList);
-    
-    checkInputValidity(inputElement, errorElement, invalidInputClass);
-    toggleButtonState(formSubmitButtonElement, disableSubmitButtonClass, buttonState);
-  }
-  
-  const handleFormSubmit = (evt) => {
-    evt.preventDefault();
-  }
-  
-  const enableValidation = ({ 
-    formSelector, 
-    inputSelector,
-    invalidInputClass, 
-    submitButtonSelector, 
-    disableSubmitButtonClass 
-    }) => {
-    const formList =  Array.from(document.querySelectorAll(formSelector))
-    formList.forEach((form) => {
-        const inputList = Array.from(form.querySelectorAll(inputSelector));
-        const formSubmitButtonElement = form.querySelector(submitButtonSelector)
-        inputList.forEach((inputElement) => {
-            inputElement.addEventListener("input", (evt) =>  handlerFormInput(
-              evt, form, invalidInputClass, formSubmitButtonElement, disableSubmitButtonClass, inputList))
-          })
-          
-          form.addEventListener("submit", handleFormSubmit);
-    })
-    
-  }
-  
-  enableValidation({
-    formSelector: ".popup__form",
-    inputSelector: ".popup__input",
-    invalidInputClass: "popup__input_type_invalid",
-    submitButtonSelector: ".popup__submit-btn",
-    disableSubmitButtonClass: "popup__button_disabled"
-  });
+  inputElement.classList.add(invalidInputClass);
+  errorElement.textContent = inputElement.validationMessage;
+};
 
-  
- 
-  
+const hideInputError = (inputElement, errorElement, invalidInputClass) => {
+  inputElement.classList.remove(invalidInputClass);
+  errorElement.textContent = inputElement.validationMessage;
+};
+
+const disableButton = (formSubmitButtonElement, disableSubmitButtonClass) => {
+  formSubmitButtonElement.classList.add(disableSubmitButtonClass);
+  formSubmitButtonElement.disabled = true;
+};
+
+const enableButton = (formSubmitButtonElement, disableSubmitButtonClass) => {
+  formSubmitButtonElement.classList.remove(disableSubmitButtonClass);
+  formSubmitButtonElement.disabled = false;
+};
+
+const toggleButtonState = (
+  formSubmitButtonElement,
+  disableSubmitButtonClass,
+  buttonState
+) => {
+  if (buttonState) {
+    disableButton(formSubmitButtonElement, disableSubmitButtonClass);
+  } else {
+    enableButton(formSubmitButtonElement, disableSubmitButtonClass);
+  }
+};
+
+const checkInputValidity = (inputElement, errorElement, invalidInputClass) => {
+  if (inputElement.validity.valid) {
+    hideInputError(inputElement, errorElement, invalidInputClass);
+  } else {
+    showInputError(inputElement, errorElement, invalidInputClass);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((input) => !input.validity.valid);
+};
+
+const handleFormInput = (
+  evt,
+  form,
+  invalidInputClass,
+  formSubmitButtonElement,
+  disableSubmitButtonClass,
+  inputList,
+  inputErrorClassTemplate
+) => {
+  const inputElement = evt.target;
+  const errorElement = form.querySelector(
+    `${inputErrorClassTemplate}${inputElement.name}`
+  );
+  const buttonState = hasInvalidInput(inputList);
+
+  checkInputValidity(inputElement, errorElement, invalidInputClass);
+  toggleButtonState(
+    formSubmitButtonElement,
+    disableSubmitButtonClass,
+    buttonState
+  );
+};
+
+const enableValidation = ({
+  formSelector,
+  inputSelector,
+  invalidInputClass,
+  submitButtonSelector,
+  disableSubmitButtonClass,
+  inputErrorClassTemplate,
+}) => {
+  const formList = Array.from(document.querySelectorAll(formSelector));
+  formList.forEach((form) => {
+    const inputList = Array.from(form.querySelectorAll(inputSelector));
+    const formSubmitButtonElement = form.querySelector(submitButtonSelector);
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", (evt) =>
+        handleFormInput(
+          evt,
+          form,
+          invalidInputClass,
+          formSubmitButtonElement,
+          disableSubmitButtonClass,
+          inputList,
+          inputErrorClassTemplate
+        )
+      );
+    });
+  });
+};
+
+enableValidation({
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  invalidInputClass: "popup__input_type_invalid",
+  submitButtonSelector: ".popup__submit-btn",
+  disableSubmitButtonClass: "popup__submit-btn_disabled",
+  inputErrorClassTemplate: ".popup__input-error_field_",
+});
